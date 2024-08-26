7844813c0eba9755095b90748cbf28484810d1b8
Jose Baron <joseobaron@gmail.com>
2024-08-25 22:03:01 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,24 @@
-console.log('Hello World!')
+console.log("Hello from inside the main.js file");
+
+//let myVar;
+
+const myVar = 42;
+
+const myVarType = typeof myVar;
+
+console.log("myVarType" + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+
+
+
+function runNow () {
+    if (myVarType === "number") {
+        console.log(`will 13 this one run?`);
+      } else {
+        console.log(`will 15 this one run?`);
+      }
+}
+
+runNow();
+runNow();
