69ca0f427bac6e42332fa9112b148a240290036b
Jose Baron <joseobaron@gmail.com>
2024-08-29 23:03:33 -0700

    working code

--- w3/main.js
+++ w3/main.js
@@ -20,9 +20,37 @@ function determinePoints(numberInHousehold) {
   );
 }
 
+
+
+
+
+function houseSize(size) {
+    console.log("inside house function")
+  switch (size) {
+    case "Large House":
+      carbonFootprintPoints += 10;
+      break;
+    case "Medium House":
+      carbonFootprintPoints += 7;
+      break;
+    case "Small House":
+      carbonFootprintPoints += 4;
+      break;
+    case "Apartment":
+      carbonFootprintPoints += 2;
+      break;
+    default:
+        console.log("Invalid House Size");
+  }
+  console.log(`Because is a ${size}, total carbonfootprint points is ${carbonFootprintPoints}`)
+}
+
 let carbonFootprintPoints = 0;
 const numberInHousehold = 9;
+const size = "Medium House"
+
 
 // global scope
 determinePoints(3);
-determinePoints(4);
+//determinePoints(4);
+houseSize(size)
