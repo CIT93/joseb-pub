b805cfd580b5c9444d400573bf45228df2dd8847
Jose Baron <joseobaron@gmail.com>
2024-09-21 20:43:57 -0700

    setup done

---
+++ w7/main.js
@@ -0,0 +1,71 @@
+import { rendertTbl} from "./render.js";
+
+const FORM = document.getElementById("form");
+const cfpData = [];
+const OUTPUT = document.getElementById("output");
+
+function determineHouseSizePts(size) {
+  let houseSizePTS;
+  if (size === "large") {
+    houseSizePTS = 10;
+  } else if (size === "medium") {
+    houseSizePTS = 7;
+  } else if (size === "small") {
+    houseSizePTS = 4;
+  } else if (size === "apt") {
+    houseSizePTS = 2;
+  }
+  return houseSizePTS;
+}
+
+function determinePoints(numberInHousehold) {
+  let houseHoldPoints = 0;
+  if (numberInHousehold === 1) {
+    houseHoldPoints = 14;
+  } else if (numberInHousehold === 2) {
+    houseHoldPoints = 12;
+  } else if (numberInHousehold === 3) {
+    houseHoldPoints = 10;
+  } else if (numberInHousehold === 4) {
+    houseHoldPoints = 8;
+  } else if (numberInHousehold === 5) {
+    houseHoldPoints = 6;
+  } else if (numberInHousehold === 6) {
+    houseHoldPoints = 4;
+  } else if (numberInHousehold > 6) {
+    houseHoldPoints = 2;
+  }
+  return houseHoldPoints;
+}
+
+function start(householdNumbers, houseSize) {
+  const houseHoldPTS = determinePoints(householdNumbers);
+  const houseSizePTS = determineHouseSizePts(houseSize);
+  const total = houseHoldPTS + houseSizePTS;
+
+  cfpData.push({
+    fNameObj: FORM.firstname.value,
+    lNameObj: FORM.lastname.value,
+    householdNumberObj: householdNumbers,
+    houseSizeObj: houseSize,
+    houseHoldPTSObj: houseHoldPTS,
+    houseSizePTSObj: houseSizePTS,
+    totalObj: total,
+  });
+}
+
+FORM.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const firstName = FORM.firstname.value;
+  const lastName = FORM.lastname.value;
+  const houseMembers = parseInt(FORM.housem.value);
+  const houseSize = FORM.houses.value;
+  start(houseMembers, houseSize);
+  OUTPUT.innerHTML = "";
+  rendertTbl(cfpData);
+  FORM.reset();
+});
+
+// the exposure to modules slowly brings back memories from other programing languages
+// having import functions from another file i seen it many times and now that we
+// use it here Im sure it will create a clean code.
