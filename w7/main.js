b34017cde3731d7ba8cd07538ac50df1575a5a8b
Jose Baron <joseobaron@gmail.com>
2024-09-21 20:48:42 -0700

    challenge 1

--- w7/main.js
+++ w7/main.js
@@ -1,42 +1,8 @@
 import { rendertTbl} from "./render.js";
-
+import { determineHouseSizePts, determinePoints } from "./cfModule.js";
 const FORM = document.getElementById("form");
 const cfpData = [];
 const OUTPUT = document.getElementById("output");
-
-function determineHouseSizePts(size) {
-  let houseSizePTS;
-  if (size === "large") {
-    houseSizePTS = 10;
-  } else if (size === "medium") {
-    houseSizePTS = 7;
-  } else if (size === "small") {
-    houseSizePTS = 4;
-  } else if (size === "apt") {
-    houseSizePTS = 2;
-  }
-  return houseSizePTS;
-}
-
-function determinePoints(numberInHousehold) {
-  let houseHoldPoints = 0;
-  if (numberInHousehold === 1) {
-    houseHoldPoints = 14;
-  } else if (numberInHousehold === 2) {
-    houseHoldPoints = 12;
-  } else if (numberInHousehold === 3) {
-    houseHoldPoints = 10;
-  } else if (numberInHousehold === 4) {
-    houseHoldPoints = 8;
-  } else if (numberInHousehold === 5) {
-    houseHoldPoints = 6;
-  } else if (numberInHousehold === 6) {
-    houseHoldPoints = 4;
-  } else if (numberInHousehold > 6) {
-    houseHoldPoints = 2;
-  }
-  return houseHoldPoints;
-}
 
 function start(householdNumbers, houseSize) {
   const houseHoldPTS = determinePoints(householdNumbers);
